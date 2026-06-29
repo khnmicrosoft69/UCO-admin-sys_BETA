57e20ae2584da6a3c7007db488392d6a9d1a04f6
Khan <ferzkhan668@gmail.com>
2026-06-30 01:01:58 +0800

    3rd commit: vercel adapting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   // We only use the React integration here
   integrations: [react()],
   vite: {
